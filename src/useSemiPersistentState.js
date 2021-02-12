6a9d5b9dc6cfc5e05c24de4dfc140a1f068eb4af
JZachrisson <jesper.zachrisson@gmail.com>
2021-02-12 09:00:21 +0100

    add useref to customstate hook

--- src/useSemiPersistentState.js
+++ src/useSemiPersistentState.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const useSemiPersistentState = (key, initialState) => {
+  const isMounted = React.useRef(false);
+
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
   );
 
   React.useEffect(() => {
-    localStorage.setItem(key, value);
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      localStorage.setItem(key, value);
+    }
   }, [value, key]);
 
   return [value, setValue];
