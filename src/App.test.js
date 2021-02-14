049969c9a792c4cd5a044baf9d31bb647def718e
JZachrisson <jesper.zachrisson@gmail.com>
2021-02-14 15:51:41 +0100

    add tests for item, list and searchform

--- src/App.test.js
+++ src/App.test.js
@@ -17,9 +17,17 @@ describe('Item', () => {
     objectID: 0,
   };
 
-  it('renders all properties', () => {
-    const component = renderer.create(<Item item={item} />);
+  const handleRemoveItem = jest.fn();
 
+  let component;
+
+  beforeEach(() => {
+    component = renderer.create(
+      <Item item={item} onRemoveItem={handleRemoveItem} />
+    );
+  });
+
+  it('renders all properties', () => {
     expect(component.root.findByType('a').props.href).toEqual(
       'https://reactjs.org/'
     );
@@ -27,5 +35,85 @@ describe('Item', () => {
     expect(component.root.findAllByProps({ children: 'React' }).length).toEqual(
       1
     );
+  });
+
+  it('calls onRemoveIten on button click', () => {
+    component.root.findByType('button').props.onClick();
+
+    expect(handleRemoveItem).toHaveBeenCalledTimes(1);
+    expect(handleRemoveItem).toHaveBeenCalledWith(item);
+
+    expect(component.root.findAllByType(Item).length).toEqual(1);
+  });
+});
+
+describe('List', () => {
+  const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  it('renders two times', () => {
+    const component = renderer.create(<List list={list} />);
+
+    expect(component.root.findAllByType(Item).length).toEqual(2);
+  });
+});
+
+describe('SearchForm', () => {
+  const searchFormProps = {
+    searchTerm: 'React',
+    onSearchInput: jest.fn(),
+    onSearchSubmit: jest.fn(),
+  };
+
+  let component;
+
+  beforeEach(() => {
+    component = renderer.create(<SearchForm {...searchFormProps} />);
+  });
+
+  it('renders the input field with its value', () => {
+    const value = component.root.findByType('input').props.value;
+
+    expect(value).toEqual('React');
+  });
+
+  it('changes the input field', () => {
+    const pseudoEvent = { target: 'Redux' };
+
+    component.root.findByType('input').props.onChange(pseudoEvent);
+
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it('submits the form', () => {
+    const pseudoEvent = {};
+
+    component.root.findByType('form').props.onSubmit(pseudoEvent);
+
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it('disables the button and prevents submit', () => {
+    component.update(<SearchForm {...searchFormProps} searchTerm="" />);
+
+    expect(component.root.findByType('button').props.disabled).toBeTruthy();
   });
 });
